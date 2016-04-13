9992447389c64ce0634a0daca3190eb4bbef6528
Gamadril <gamadril.dev@gmail.com>
2016-04-13 17:04:31 +0200

    Made resetPin parameter optional in configurePins() for the case if only CS pin should be changed

--- src/DDBooster.h
+++ src/DDBooster.h
@@ -59,7 +59,7 @@ public:
      * @param csPin - Digital Arduino pin to use as chip select
      * @param resetPin - Digital Arduino pin connected to the RESET pin of the DD-Booster
      */
-    void configurePins(uint8_t csPin, uint8_t resetPin);
+    void configurePins(uint8_t csPin, uint8_t resetPin = 0xFF);
 
     /**
      * Performs initial configuration of the DD-Booster to set the number of used LEDs and their type.
