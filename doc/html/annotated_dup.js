3a3abec15825f376d1178ab57085e38a2df22633
Gamadril <gamadril.dev@gmail.com>
2022-03-18 12:02:04 +0100

    Version update to 1.0.2 Changes: - setRange allows wrap around if start > end

--- doc/html/annotated_dup.js
+++
@@ -1,4 +0,0 @@
-var annotated_dup =
-[
-    [ "DDBooster", "class_d_d_booster.html", "class_d_d_booster" ]
-];
--- doc/html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['ddbooster_2eh',['DDBooster.h',['../_d_d_booster_8h.html',1,'']]]
-];
